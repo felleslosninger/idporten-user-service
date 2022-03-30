e29dcddf8cfba22dfdc291659052d9547d32c8dc
Thomas Reppesgård <thr@digdir.no>
2022-03-30 12:21:44 +0200

    spring initializer

---
+++ src/test/java/no/idporten/idportenuserservice/IdportenUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.idporten.idportenuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdportenUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
