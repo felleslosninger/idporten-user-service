27a80afdd5c51e42d5d5f8c48d37249912eb93a6
Randi Øyri <randi.oyri@digdir.no>
2022-03-30 13:32:46 +0200

    ID:1493: initielle kall

--- src/main/java/no/idporten/userservice/api/UserController.java
+++ src/main/java/no/idporten/userservice/api/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @Controller("/user")
