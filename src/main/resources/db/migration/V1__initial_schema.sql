cd639cf0b5e319fae6b4933c994196cefebb489c
Thomas Reppesgård <thr@digdir.no>
2022-03-30 12:45:00 +0200

    H2, flyway

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS `idporten_user`
 (
     `uuid`              VARCHAR ( 128 ) NOT NULL,
     `person_identifier` VARCHAR ( 255 ) NOT NULL,
-    PRIMARY KEY ( `uuid` ),
+    PRIMARY KEY ( `uuid` )
 );
