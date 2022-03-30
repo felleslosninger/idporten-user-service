8d61fa73e5244293e8b971ef7c2c04c48e5126d0
Thomas Reppesgård <thr@digdir.no>
2022-03-30 13:50:01 +0200

    Fix controller

--- src/main/java/no/idporten/userservice/api/UserController.java
+++ src/main/java/no/idporten/userservice/api/UserController.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import no.idporten.userservice.data.IDPortenUser;
 import no.idporten.userservice.data.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -17,6 +19,6 @@ public class UserController {
     @GetMapping
     public ResponseEntity<IDPortenUser> getUser(@RequestParam String pid){
         IDPortenUser user = userService.findUser(pid);
-        return ResponseEntity.ok().body(user);
+        return ResponseEntity.ok(user);
     }
 }
