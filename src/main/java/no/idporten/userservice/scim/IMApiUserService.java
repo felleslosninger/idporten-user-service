804144d72ba37b1551677dddb1d509b18dc9a971
Thomas Reppesgård <thr@digdir.no>
2022-04-19 14:31:56 +0200

    wip

--- src/main/java/no/idporten/userservice/scim/IMApiUserService.java
+++ src/main/java/no/idporten/userservice/scim/IMApiUserService.java
@@ -1,8 +1,8 @@
 package no.idporten.userservice.scim;
 
 import lombok.RequiredArgsConstructor;
-import no.idporten.scim.api.*;
-import no.idporten.scim.spi.ScimUserService;
+import no.idporten.im.api.*;
+import no.idporten.im.spi.IDPortenIdentityManagementUserService;
 import no.idporten.userservice.data.EID;
 import no.idporten.userservice.data.IDPortenUser;
 import no.idporten.userservice.data.UserService;
@@ -12,6 +12,9 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -20,25 +23,25 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Primary
 @Service
-public class IDPortenScimUserService implements ScimUserService {
+public class IMApiUserService implements IDPortenIdentityManagementUserService {
 
     private final UserService userService;
 
     @Override
-    public ScimUserResource lookup(String userId) {
+    public UserResource lookup(String userId) {
         IDPortenUser idPortenUser = userService.findUser(UUID.fromString(userId));
         System.out.println("idPortenUser.getEids() = " + idPortenUser.getEids());
-        ScimUserResource scimUserResource = convert(idPortenUser);
+        UserResource scimUserResource = convert(idPortenUser);
         return scimUserResource;
     }
 
     @Override
-    public List<ScimUserResource> searchForUser(String personIdentifier) {
+    public List<UserResource> searchForUser(String personIdentifier) {
         return userService.searchForUser(personIdentifier).stream().map(this::convert).collect(Collectors.toList());
     }
 
     @Override
-    public ScimUserResource createUserOnFirstLogin(CreateUserRequest createUserRequest) {
+    public UserResource createUserOnFirstLogin(CreateUserRequest createUserRequest) {
         IDPortenUser idPortenUser = new IDPortenUser();
         idPortenUser.setPid(createUserRequest.getPersonIdentifier());
         idPortenUser.setActive(true);
@@ -46,12 +49,12 @@ public class IDPortenScimUserService implements ScimUserService {
     }
 
     @Override
-    public ScimUserResource updateUserLogins(String userId, UpdateUserLoginRequest updateUserLoginRequest) {
+    public UserResource updateUserLogins(String userId, UpdateUserLoginRequest updateUserLoginRequest) {
         return convert(userService.updateUserWithEid(UUID.fromString(userId), EID.builder().name(updateUserLoginRequest.getEidName()).build()));
     }
 
     @Override
-    public ScimUserResource updateUserStatus(String userId, UpdateUserStatusRequest updateUserStatusRequest) {
+    public UserResource updateUserStatus(String userId, UpdateUserStatusRequest updateUserStatusRequest) {
         IDPortenUser idPortenUser = userService.findUser(UUID.fromString(userId));
         idPortenUser.setCloseCode(updateUserStatusRequest.getClosedCode());
         idPortenUser.setCloseCodeLastUpdated(Clock.systemUTC().instant());
@@ -62,31 +65,46 @@ public class IDPortenScimUserService implements ScimUserService {
     }
 
     @Override
-    public ScimUserResource changePersonIdentifier(ChangePersonIdentifierRequest changePersonIdentifierRequest) {
+    public UserResource changePersonIdentifier(ChangePersonIdentifierRequest changePersonIdentifierRequest) {
         return null;
     }
 
-    private ScimUserResource convert(IDPortenUser idPortenUser) {
-        ScimUserResource scimUserResource = new ScimUserResource();
-        scimUserResource.setId(idPortenUser.getId().toString());
-        scimUserResource.setActive(idPortenUser.isActive());
-        scimUserResource.setPersonIdentifier(idPortenUser.getPid());
+    protected UserResource convert(IDPortenUser idPortenUser) {
+        UserResource userResource = new UserResource();
+        userResource.setId(idPortenUser.getId().toString());
+        userResource.setActive(idPortenUser.isActive());
+        userResource.setPersonIdentifier(idPortenUser.getPid());
         if (StringUtils.hasText(idPortenUser.getCloseCode())) {
-            scimUserResource.setUserStatus(new UserStatus(idPortenUser.getCloseCode()));
+            UserStatus userStatus = new UserStatus();
+            userStatus.setClosedCode(userStatus.getClosedCode());
+            userStatus.setClosedDate(convert(idPortenUser.getCloseCodeLastUpdated()));
+            userResource.setUserStatus(userStatus);
         }
-        scimUserResource.setUserLogins(convertUserLogins(idPortenUser));
-        return scimUserResource;
+        userResource.setUserLogins(convertUserLogins(idPortenUser));
+        return userResource;
     }
 
-    private UserLogin convert(EID eid) {
-        return new UserLogin(eid.getName());
+    protected UserLogin convert(EID eid) {
+        UserLogin userLogin = new UserLogin();
+        userLogin.setEid(eid.getName());
+        userLogin.setFirstLogin(convert(eid.getFirstLogin()));
+        userLogin.setLastLogin(convert(eid.getLastLogin()));
+        return userLogin;
     }
 
-    private List<UserLogin> convertUserLogins(IDPortenUser idPortenUser) {
+    protected List<UserLogin> convertUserLogins(IDPortenUser idPortenUser) {
         if (CollectionUtils.isEmpty(idPortenUser.getEids())) {
             return Collections.emptyList();
         }
         return idPortenUser.getEids().stream().map(this::convert).toList();
     }
+
+    protected ZonedDateTime convert(Instant instant) {
+        if (instant == null) {
+            return null;
+        }
+        return ZonedDateTime.ofInstant(instant, ZoneId.systemDefault());
+    }
+
 
 }
