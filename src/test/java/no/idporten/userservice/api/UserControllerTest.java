26243ee68cbdcaca868dda15fc9baf6c636c954a
Randi Øyri <randi.oyri@digdir.no>
2022-05-05 10:29:06 +0200

    ID-1495: Use test-profile

--- src/test/java/no/idporten/userservice/api/UserControllerTest.java
+++ src/test/java/no/idporten/userservice/api/UserControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @DisplayName("When using the user API")
+@ActiveProfiles("test")
 public class UserControllerTest {
 
     @Autowired
