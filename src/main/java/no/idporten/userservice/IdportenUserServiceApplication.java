a75433ac95d87cd3ee5bb780dded3c626e81ef2d
Randi Øyri <randi.oyri@digdir.no>
2022-06-22 10:10:10 +0200

    ID-1848: rydde i imports

--- src/main/java/no/idporten/userservice/IdportenUserServiceApplication.java
+++ src/main/java/no/idporten/userservice/IdportenUserServiceApplication.java
@@ -1,9 +1,7 @@
 package no.idporten.userservice;
 
 import no.idporten.logging.access.AccessLogsConfiguration;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
