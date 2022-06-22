d4397a4f20b5e5a71a69f56b96c8e48dcbe13509
Randi Øyri <randi.oyri@digdir.no>
2022-06-22 10:57:25 +0200

    ID-1848: ny versjon av lib utan conditional bean

--- src/main/java/no/idporten/userservice/IdportenUserServiceApplication.java
+++ src/main/java/no/idporten/userservice/IdportenUserServiceApplication.java
@@ -1,12 +1,9 @@
 package no.idporten.userservice;
 
-import no.idporten.logging.access.AccessLogsConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({ AccessLogsConfiguration.class})
 public class IdportenUserServiceApplication {
 
 	public static void main(String[] args) {
