9134a36d3b9dbecda44939f47bb359f28bc56e6d
kons-ahj <opusmarta@gmail.com>
2022-11-07 13:31:25 +0100

    actuator lib

--- src/main/java/no/idporten/userservice/config/SwaggerConfig.java
+++ src/main/java/no/idporten/userservice/config/SwaggerConfig.java
@@ -3,19 +3,23 @@ package no.idporten.userservice.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI(@Value("${project.version}") String appVersion) {
+    public OpenAPI customOpenAPI(Optional<BuildProperties> buildProperties) {
         return new OpenAPI()
                 .info(new Info()
                         .title("ID-porten user service")
-                        .version(appVersion)
+                        .version(buildProperties.isPresent() ?
+                                buildProperties.get().getVersion() :
+                                "unknown")
                         .description("ID-porten user service"))
                 .components(new Components().addSecuritySchemes("basic_auth", securitySchemeBasicAuth()).addSecuritySchemes("access_token", securitySchemeBearer()));
     }
@@ -23,6 +27,7 @@ public class SwaggerConfig {
     private io.swagger.v3.oas.models.security.SecurityScheme securitySchemeBearer() {
         return new io.swagger.v3.oas.models.security.SecurityScheme().type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("access_token");
     }
+
     private io.swagger.v3.oas.models.security.SecurityScheme securitySchemeBasicAuth() {
         return new io.swagger.v3.oas.models.security.SecurityScheme().type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP).scheme("basic");
     }
---
+++ src/test/java/no/idporten/userservice/api/ActuatorTest.java
@@ -0,0 +1,46 @@
+package no.idporten.userservice.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("When accessing the version endpoint")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ActuatorTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext springContext;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(springContext).build();
+    }
+
+    @Test
+    @DisplayName("then the json response contains a version attribute")
+    void testGetVersion() throws Exception {
+        mockMvc.perform(get("/version"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.version").exists());
+    }
+
+    @Test
+    @DisplayName("then the json response contains a java attribute")
+    void testGetInfo() throws Exception {
+        mockMvc.perform(get("/info"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.java").exists());
+    }
+
+}
