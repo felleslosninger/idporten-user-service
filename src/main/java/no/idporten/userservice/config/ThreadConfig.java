2de18bfcfc60784d91546e83cc65cf38fc8c8714
Sigvald Høyheim <sigvald.hoyheim@digdir.no>
2023-11-08 08:06:11 +0100

    Legg til Virtual Threads config

---
+++ src/main/java/no/idporten/userservice/config/ThreadConfig.java
@@ -0,0 +1,32 @@
+package no.idporten.userservice.config;
+
+import java.util.concurrent.Executors;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+@ConditionalOnProperty(
+  value = "spring.thread-executor",
+  havingValue = "virtual"
+)
+public class ThreadConfig {
+
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
+    }
+}
