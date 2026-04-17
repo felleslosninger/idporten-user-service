687a4da7cfa4ea320686c34c874fe4c394f0e9b1
Anne Marte Hjemås <anne.marte.hjemaas@digdir.no>
2026-04-17 13:19:30 +0200

    Update Redis test configuration and bump dependencies

--- src/test/java/no/idporten/userservice/BaseRedisTest.java
+++ src/test/java/no/idporten/userservice/BaseRedisTest.java
@@ -7,23 +7,40 @@ import org.springframework.test.context.DynamicPropertySource;
 import redis.embedded.RedisServer;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 
 public abstract class BaseRedisTest {
 
     private static RedisServer redisServer;
 
+    private static int redisPort;
+
     @BeforeAll
     static void startRedisServer() throws IOException{
         if (redisServer == null) {
-            redisServer = new RedisServer();
+            redisPort = findFreePort();
+            redisServer = new RedisServer(redisPort);
             redisServer.start();
+        }
+    }
+
+    @AfterAll
+    static void stopRedisServer() throws IOException {
+        if (redisServer != null) {
+            redisServer.stop();
         }
     }
 
     @DynamicPropertySource
     static void registerRedisProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.redis.port", () -> "6379");
+        registry.add("spring.data.redis.port", () -> String.valueOf(redisPort));
         registry.add("spring.data.redis.host", () -> "localhost");
+    }
+
+    private static int findFreePort() throws IOException {
+        try (ServerSocket socket = new ServerSocket(0)) {
+            return socket.getLocalPort();
+        }
     }
 
 }
